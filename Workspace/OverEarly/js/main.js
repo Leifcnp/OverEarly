083936e1b8977e8ac2bcd10d31ddb4f01aa5f4c7
Leif Pedersen <leifcnp@gmail.com>
2017-05-16 07:09:35 -0700

    timer system counting

--- Workspace/OverEarly/js/main.js
+++ Workspace/OverEarly/js/main.js
@@ -1,4 +1,5 @@
 
+ 
 window.onload = function () {
     // TODO:: Do your initialization job
 
@@ -11,12 +12,59 @@ window.onload = function () {
 			}
 		}
 	});
+    
+ // add eventListener for tizenhwkey
+    document.addEventListener('tizenhwkey', function(e) {
+        if(e.keyName === "back") {
+			try {
+			    tizen.application.getCurrentApplication().exit();
+			} catch (ignore) {
+				
+			}
+		}
+	});
 
-    // Sample code
+    var firstHorn= new Date().getTime();
+    var startLength = 1;
+    var startLengthInMilli = startLength*60*1000 + 1000;
     var textbox = document.querySelector('.contents');
+    let startSequence = false;
+    
     textbox.addEventListener("click", function(){
-    	var box = document.querySelector('#textbox');
-    	box.innerHTML = (box.innerHTML === "Basic") ? "Sample" : "Basic";
+    	var firstHorn= new Date().getTime();
+    	startSequence = true;
+    	console.log("Hey there, Looks like samsung knows something...")
+    	//var box = document.querySelector('#textbox');
+    	//box.innerHTML = (box.innerHTML === "Basic") ? "Sample" : "Basic";
+    	
     });
     
+    
+    
+    setInterval(function() {
+    		if(startSequence){
+    			if(true){
+    				var lastHorn = firstHorn + startLengthInMilli;
+    			}
+    			  var currentTime = new Date().getTime();
+
+    	    	  var timeLeft =   lastHorn - currentTime;
+    	    	  
+    	    	  var minutes = Math.floor((timeLeft % (1000 * 360)) / (60000));
+    	    	  var seconds = Math.floor( (timeLeft % (1000 * 60))  / 1000);
+
+    	    	  document.getElementById("Timer").innerHTML = + minutes + "m " + seconds + "s ";
+
+    	    	  if (timeLeft <= 0) {
+    	    	    document.getElementById("Timer").innerHTML = "GO!";
+    	    	  }
+    		}else{
+    			document.getElementById("Timer").innerHTML = "Tap on First Horn!";
+    			
+    		}
+    	  
+    	}, 100);
+    
+    
 };
+
